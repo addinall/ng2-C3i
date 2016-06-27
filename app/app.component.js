3a96a0ac0d43f59bcd9363700a890d738f447bc8
addinall <addinall@addinall.net>
2016-06-28 08:26:19 +1000

    Breakfast push, installed Maps API.  NEAT!

--- app/app.component.js
+++ app/app.component.js
@@ -1,3 +1,17 @@
+// CAPTAIN SLOG
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// File         : app.components.ts 
+// System       : ng2-C3I 
+// Date         : June  2016
+// Author       : Mark Addinall
+// Synopsis     : This system is the clone of C3I using newer technology.
+//                Although C3I is using VERY up to date tools and design
+//                coding concepts, this takes R&D a step further by
+//                basing the architecture of this version around
+//                Angular2 and Bootstrap4.
+//
+//
+// -------------------------------
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -8,18 +22,50 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
+var core_1 = require('@angular/core'); // This is the base of the Angular magic.
+// near EVERYTHING in an Angular app is
+// a component 
+var Asset = (function () {
+    function Asset() {
+    }
+    return Asset;
+}());
+exports.Asset = Asset;
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'ng2-C3I COMMAND and CONTROL'; // visible to all.
+        this.assets = ASSETS;
+        this.asset = {
+            id: 10.,
+            name: 'Speedometer',
+            owner: 'Addinall'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
+            // All iof this CSS is going to live in it's own
+            // seperate file by the end of the day.  Just in
+            // anyone is having a peek.
             selector: 'my-app',
-            template: '<h1>ng2-C3I COMMAND and CONTROL - Addinall</h1>'
+            styles: ["\n                    .selected {\n                        background-color: #CFD8DC !important;\n                        color: white;\n                    }\n                    .assets {\n                        margin: 0 0 2em 0;\n                        list-style-type: none;\n                        padding: 0;\n                        width: 15em;\n                    }\n                    .assets li {\n                        cursor: pointer;\n                        position: relative;\n                        left: 0;\n                        background-color: #EEE;\n                        margin: .5em;\n                        padding: .3em 0;\n                        height: 1.6em;\n                        border-radius: 4px;\n                    }\n                    .assets li.selected:hover {\n                        background-color: #BBD8DC !important;\n                        color: white;\n                    }\n                    .assets li:hover {\n                        color: #607D8B;\n                        background-color: #DDD;\n                        left: .1em;\n                    }\n                    .assets .text {\n                        position: relative;\n                        top: -3px;\n                    }\n                    .assets .badge {\n                        display: inline-block;\n                        font-size: small;\n                        color: white;\n                        padding: 0.8em 0.7em 0 0.7em;\n                        background-color: #607D8B;\n                        line-height: 1em;\n                        position: relative;\n                        left: -1px;\n                        top: -4px;\n                        height: 1.8em;\n                        margin-right: .8em;\n                        border-radius: 4px 0 0 4px;\n                    }\n                "],
+            template: "<h1>{{title}}</h1>\n                    <h2>Active Assets</h2>\n                    <ul class=\"assets\">\n                        <li *ngFor=\"let asset of assets\">\n                            <span class=\"state\"> {{asset.id}}</span><span> {{asset.name}}</span> {{asset.owner}}\n                        </li>\n                    </ul>\n                    <h2>{{asset.name}} Details.</h2> \n                    <h3>{{asset.owner}} Owner.</h3> \n                    <div><label>id: </label>{{asset.id}}</div>\n                    <div>\n                        <label>Owner: </label>\n                        <input [(ngModel)]=\"asset.owner\" placeholder=\"name\">\n                    </div>\n                    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var ASSETS = [
+    { id: 11, name: 'Barometer', owner: 'Addinall' },
+    { id: 12, name: 'Thermometer', owner: 'Addinall' },
+    { id: 13, name: 'Bettsometer', owner: 'Addinall' },
+    { id: 14, name: 'Actinometer', owner: 'Smith' },
+    { id: 15, name: 'Densimeter', owner: 'Smith' },
+    { id: 16, name: 'Dynometer', owner: 'Smith' },
+    { id: 17, name: 'Fathometer', owner: 'DuPont' },
+    { id: 18, name: 'Hygrometer', owner: 'DuPont' },
+    { id: 19, name: 'Magnetometer', owner: 'DuPont' },
+    { id: 20, name: 'Manometer', owner: 'Tynes' }
+];
+// --------------------------  EOF --------------------------
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,124 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'my-app',
-  template: '<h1>ng2-C3I COMMAND and CONTROL - Addinall</h1>'
-})
-export class AppComponent { }
+// CAPTAIN SLOG
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// File         : app.components.ts 
+// System       : ng2-C3I 
+// Date         : June  2016
+// Author       : Mark Addinall
+// Synopsis     : This system is the clone of C3I using newer technology.
+//                Although C3I is using VERY up to date tools and design
+//                coding concepts, this takes R&D a step further by
+//                basing the architecture of this version around
+//                Angular2 and Bootstrap4.
+//
+//
+// -------------------------------
+
+    import { Component } from '@angular/core';                  // This is the base of the Angular magic.
+                                                                // near EVERYTHING in an Angular app is
+                                                                // a component 
+    export class Asset {                                        // In this syntax of JS, we have                               
+        id:     number;                                         // introduced the classic representation
+        name:   string;                                         // of application 'objects'.  Some don't
+        owner:  string;                                         // like this.  Easier to read than closuers I think.
+    }
+
+    @Component({                                                // set up THIS Component
+                                                                // All iof this CSS is going to live in it's own
+                                                                // seperate file by the end of the day.  Just in
+                                                                // anyone is having a peek.
+        selector: 'my-app',
+        styles: [`
+                    .selected {
+                        background-color: #CFD8DC !important;
+                        color: white;
+                    }
+                    .assets {
+                        margin: 0 0 2em 0;
+                        list-style-type: none;
+                        padding: 0;
+                        width: 15em;
+                    }
+                    .assets li {
+                        cursor: pointer;
+                        position: relative;
+                        left: 0;
+                        background-color: #EEE;
+                        margin: .5em;
+                        padding: .3em 0;
+                        height: 1.6em;
+                        border-radius: 4px;
+                    }
+                    .assets li.selected:hover {
+                        background-color: #BBD8DC !important;
+                        color: white;
+                    }
+                    .assets li:hover {
+                        color: #607D8B;
+                        background-color: #DDD;
+                        left: .1em;
+                    }
+                    .assets .text {
+                        position: relative;
+                        top: -3px;
+                    }
+                    .assets .badge {
+                        display: inline-block;
+                        font-size: small;
+                        color: white;
+                        padding: 0.8em 0.7em 0 0.7em;
+                        background-color: #607D8B;
+                        line-height: 1em;
+                        position: relative;
+                        left: -1px;
+                        top: -4px;
+                        height: 1.8em;
+                        margin-right: .8em;
+                        border-radius: 4px 0 0 4px;
+                    }
+                `]
+        template: `<h1>{{title}}</h1>
+                    <h2>Active Assets</h2>
+                    <ul class="assets">
+                        <li *ngFor="let asset of assets">
+                            <span class="state"> {{asset.id}}</span><span> {{asset.name}}</span> {{asset.owner}}
+                        </li>
+                    </ul>
+                    <h2>{{asset.name}} Details.</h2> 
+                    <h3>{{asset.owner}} Owner.</h3> 
+                    <div><label>id: </label>{{asset.id}}</div>
+                    <div>
+                        <label>Owner: </label>
+                        <input [(ngModel)]="asset.owner" placeholder="name">
+                    </div>
+                    `
+    })
+
+
+    export class AppComponent {                                 // and make an application component
+        title = 'ng2-C3I COMMAND and CONTROL';                  // visible to all.
+        public assets = ASSETS;
+        asset: Asset = {
+                        id: 10.
+                        name: 'Speedometer',
+                        owner: 'Addinall'
+        };
+    }
+
+
+    const ASSETS: Asset[] = [                                   // this lot is going to come out of some
+        { id: 11, name: 'Barometer',    owner: 'Addinall'   },  // sort of data store that will be provided to
+        { id: 12, name: 'Thermometer',  owner: 'Addinall'   },  // us via a RESTful server.  In this case,
+        { id: 13, name: 'Bettsometer',  owner: 'Addinall'   },  // the one I am writing in the
+        { id: 14, name: 'Actinometer',  owner: 'Smith'      },  // backend bits of code.
+        { id: 15, name: 'Densimeter',   owner: 'Smith'      },  // this stuff is for testing purposes.
+        { id: 16, name: 'Dynometer',    owner: 'Smith'      },
+        { id: 17, name: 'Fathometer',   owner: 'DuPont'     },
+        { id: 18, name: 'Hygrometer',   owner: 'DuPont'     },
+        { id: 19, name: 'Magnetometer', owner: 'DuPont'     },
+        { id: 20, name: 'Manometer',    owner: 'Tynes'      }
+    ];
+
+
+
+// --------------------------  EOF --------------------------
 
