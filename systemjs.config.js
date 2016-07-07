2d90def95d03ce5853f4c1b4d367b8e467369926
addinall <addinall@addinall.net>
2016-07-07 16:09:04 +1000

    Angular2 is still very broken. react() looks like an optio

--- systemjs.config.js
+++ systemjs.config.js
@@ -12,9 +12,9 @@
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'ts' },
+    'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' }
   };
   var ngPackageNames = [
     'common',
